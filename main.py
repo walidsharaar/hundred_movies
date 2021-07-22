588d3daa4381123396bacefa8e47e4342bc2301f
Sharaar <walidsharar@gmail.com>
2021-07-22 21:34:31 +0200

    Save the movies in a texted file

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://www.empireonline.com/movies/features/best-movies-2/"
+import requests
+from bs4 import BeautifulSoup
 
-# get request from the website
+URL = "https://www.empireonline.com/movies/features/best-movies-2/"
 
 response = requests.get(URL)
 website_html = response.text
 
-soup = BeautifulSoup(website_html,"html.parser")
-movies= soup.find_all(name="h3", class_="jsx-4245974604")
-print(movies)
+soup = BeautifulSoup(website_html, "html.parser")
 
-movie_titles = [movies.getText() for movie in movies]
+all_movies = soup.find_all(name="h3", class_="jsx-4245974604")
+
+movie_titles = [movie.getText() for movie in all_movies]
+movies = movie_titles[::-1]
+
+with open("movies.txt", mode="w") as file:
+    for movie in movies:
+        file.write(f"{movie}\n")
