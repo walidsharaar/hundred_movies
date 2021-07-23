98602af1ea8c85db43d10435416f38b76adee58c
Sharaar <walidsharar@gmail.com>
2021-07-23 10:57:45 +0200

    final modifications

--- main.py
+++ main.py
@@ -1,26 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
-
 #website Url
 URL = "https://www.empireonline.com/movies/features/best-movies-2/"
 
 response = requests.get(URL)
 website_html = response.text
 
-
 soup = BeautifulSoup(website_html, "html.parser")
-
 #get all movie data from website
 all_movies = soup.find_all(name='div',class_='jsx-4245974604')
-
 #get all movie names
 movie_titles = [movie.getText() for movie in all_movies]
-#Reverse the list order movies = movie_titles[::-1]
+#Reverse the list order
+movies = movie_titles[::-1]
 
-for n in range(len(movie_titles)-1,-1,-1):
-    print(movie_titles[n])
-
-
+#create file
 with open("movies.txt", mode="w") as file:
-    for movie in n:
+    for movie in movies:
         file.write(f"{movie}\n")
